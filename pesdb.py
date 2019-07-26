ea8e954a5390aae7ca4ef4039bd55525122ded95
Richard Cheng <richardcheng@Richards-MacBook-Pro.local>
2019-07-26 16:38:40 -0400

    began scraping script

---
+++ pesdb.py
@@ -0,0 +1,25 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import pickle
+
+players = []
+for i in range(1, 400):
+    url = "http://pesdb.net/pes2019/?page=" + str(i)
+    website = requests.get(url)
+    soup = BeautifulSoup(website.text, "lxml")
+    new_players = soup.select("a[href*=id]")
+    if not new_players:
+        print("\nstopped")
+        time.sleep(180)
+    players += new_players
+    updated_players = [str(p) for p in players if 'http' not in str(p)]
+    print(updated_players)
+    pickle.dump(updated_players, open('players.pkl', 'wb'))
+    time.sleep(0.5)
+
+
+
+
+
