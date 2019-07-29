2a58a8347df956d51b28231bec82bf602658af01
Richard Cheng <richardcheng@Richards-MacBook-Pro.local>
2019-07-29 17:35:55 -0400

    fixed players.pkl

--- pesdb.py
+++ pesdb.py
@@ -5,18 +5,21 @@ from bs4 import BeautifulSoup
 import pickle
 
 players = []
-for i in range(324,401):
+for i in range(1,401):
     url = "http://pesdb.net/pes2019/?page=" + str(i)
     website = requests.get(url)
     soup = BeautifulSoup(website.text, "lxml")
     new_players = soup.select("a[href*=id]")
     if not new_players:
         print("\nstopped")
-        time.sleep(180)
+        time.sleep(190)
+        website = requests.get(url)
+        soup = BeautifulSoup(website.text, "lxml")
+        new_players = soup.select("a[href*=id]")
     players += new_players
     updated_players = [str(p) for p in players if 'http' not in str(p)]
     print(updated_players)
-    pickle.dump(updated_players, open('players2.pkl', 'wb'))
+    pickle.dump(updated_players, open('players.pkl', 'wb'))
     time.sleep(0.5)
 
 
