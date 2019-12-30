0151c71b829546f32de3edd8602952ca6a2dbd6c
Richard Cheng <richardcheng@Richards-MacBook-Pro.local>
2019-12-30 13:47:23 -0600

    added stats df

--- hyperlinks.py
+++ hyperlinks.py
@@ -4,40 +4,51 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
 
 players = pickle.load(open("players.pkl", 'rb'))
-array = np.asarray(players)
-
-rows = []
-for i in range(0,1):
-    link = array[i]
-    numbers = [str(s) for s in link if s.isdigit()]
-    id = int("".join(numbers))
-    url = "http://pesdb.net/pes2019/?id=" + str(id)
-    website = requests.get(url)
-    soup = BeautifulSoup(website.content, "lxml")
-    table = soup.find(id="table_0")
-    tr = table.find("tr")
-    attr_row = []
-    th_content = []
-    df = pd.DataFrame(
-        columns=['Attacking Prowess:', 'Ball Control:', 'Low Pass:', 'Lofted Pass:', 'Finishing:', 'Place Kicking:',
-                 'Swerve:', 'Header:', 'Kicking Power:', 'Speed:', 'Explosive Power:', 'Unwavering Balance:',
+df = pd.DataFrame(
+        columns=['Name:', 'Attacking Prowess:', 'Ball Control:', 'Dribbling:', 'Low Pass:', 'Lofted Pass:', 'Finishing:', 'Place Kicking:',
+                 'Swerve:', 'Header:', 'Defensive Prowess:', 'Ball Winning:', 'Kicking Power:', 'Speed:', 'Explosive Power:', 'Unwavering Balance:',
                  'Physical Contact:', 'Jump:', 'Stamina:'])
-    for attribute in tr.contents:
-        attr = str(attribute)
-        name = re.findall("Player Name:</th><td>(.+?)</td>", attr)
-        if name:
-            name = name
-        match = re.findall("id=(.+?)</td></tr>", attr)
-        if match:
-            match = match
-            td_content = re.findall(">(.+?)'", str(match))
-            td_content = [x for x in td_content if "40" not in x] #removes goalkeeper stats
-            td_content = [x for x in td_content if "<" not in x]
-            td_content = [int(x) for x in td_content]
-            df.loc[name] = td_content
-            print(df)
+rows = []
+for player in players:
+    try:
+        numbers = [str(s) for s in player if s.isdigit()]
+        id = int("".join(numbers))
+        url = "http://pesdb.net/pes2019/?id=" + str(id)
+        website = requests.get(url)
+        print(website.status_code)
+        if website.status_code == 429:
+            print("stopped")
+            time.sleep(190)
+            website = requests.get(url)
+        soup = BeautifulSoup(website.content, "lxml")
+        table = soup.find(id="table_0")
+        tr = table.find("tr")
+        th_content = []
+        running_list = []
+        for attribute in tr.contents:
+            attr = str(attribute)
+            name = re.findall("Player Name:</th><td>(.+?)</td>", attr)
+            if name:
+                name = name
+                running_list.append(name)
+            match = re.findall("id=(.+?)</td></tr>", attr)
+            if match:
+                match = match
+                td_content = re.findall(">(.+?)'", str(match))
+                td_content = [x for x in td_content if "40" not in x] #removes goalkeeper stats
+                td_content = [x for x in td_content if "<" not in x]
+                td_content = [int(x) for x in td_content]
+                running_list += td_content
+
+        df.loc[len(df)] = running_list
+        print(df)
+        df.to_csv("player_stats.csv")
+    except Exception as e:
+        print(player)
+        print(e)
 
 
 
